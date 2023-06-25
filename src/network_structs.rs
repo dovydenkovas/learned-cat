9e05e9a13edee5c363a4398fee8ca47b9386be34
Dovidenkov Aleksandr <asdovydenkov@gmail.com>
2023-06-25 11:23:18 +0300

    Add structres for communication between server and client

---
+++ src/network_structs.rs
@@ -0,0 +1,37 @@
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Request {
+    pub user: String,
+    pub command: Command 
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Command {
+    GetAvaliableTests,
+    StartTest { 
+        test: String 
+    },
+    
+    GetNextQuestion { 
+        test: String,
+        previos_answer: Vec<u8>,
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Response {
+     AvaliableTests { tests: Vec<String> },
+     StartTest { banner: String },
+     GetNextQuestion { question: NextQuestion }        
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum NextQuestion {
+    TheEnd { result: String },
+    Question { 
+        question: String,
+        answers: Vec<String>
+    }
+}
